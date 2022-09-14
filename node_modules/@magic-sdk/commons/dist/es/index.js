abd7f52fecf4fc66efd2cb550b2f5835c6dcfaab
OliverMoscow <56978722+OliverMoscow@users.noreply.github.com>
2022-09-14 14:12:11 -0600

    Removed node_modules

--- node_modules/@magic-sdk/commons/dist/es/index.js
+++
@@ -1,2 +0,0 @@
-import{Extension as n,MagicSDKError as o,MagicExtensionError as a,MagicExtensionWarning as t,MagicRPCError as s,MagicSDKWarning as E,isPromiEvent as e}from"@magic-sdk/provider";export*from"@magic-sdk/types";export{n as Extension,a as ExtensionError,t as ExtensionWarning,s as RPCError,o as SDKError,E as SDKWarning,e as isPromiEvent};
-//# sourceMappingURL=index.js.map
--- node_modules/@magic-sdk/commons/dist/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Extension, MagicSDKError as SDKError, MagicExtensionError as ExtensionError, MagicExtensionWarning as ExtensionWarning, MagicRPCError as RPCError, MagicSDKWarning as SDKWarning, isPromiEvent, } from '@magic-sdk/provider';
-export type { MagicSDKAdditionalConfiguration, MagicSDKExtensionsOption, PromiEvent } from '@magic-sdk/provider';
-export * from '@magic-sdk/types';
