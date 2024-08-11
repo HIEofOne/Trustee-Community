73bba4758924627b3ea65ddb5665a10f1c6af49f
Michael Shihjay Chen <shihjay2@gmail.com>
2024-08-10 22:11:17 -0700

    update introspect

--- pages/api/as/introspect.ts
+++ pages/api/as/introspect.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import NextCors from 'nextjs-cors';
 import verifySig from '../../../lib/verifySig';
-import objectPath from 'object-path';
 
 var user = process.env.COUCHDB_USER;
 var pass = process.env.COUCHDB_PASSWORD;
@@ -27,18 +26,18 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
         "access_token.value": {"$eq": access_token}
       }
     };
-    const response = {"active": false};
     try {
       const result = await gnap.find(q);
       if (result.docs[0]) {
-        objectPath.set(response, 'active', true);
-        objectPath.set(response, 'access', result.docs[0].access);
-        res.status(200).json(response);
+        res.status(200).json({
+          "active": true,
+          "access": result.docs[0].access
+        });
       } else {
-        res.status(200).json(response);
+        res.status(200).json({"active": false});
       }
     } catch (e) {
-      res.status(200).json(response);
+      res.status(200).json({"active": false});
     }
   } else {
     res.status(401).send('Unauthorized');
