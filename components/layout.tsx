174524dfbc067a2ff4162df1f18dfde3121b2337
Michael Shihjay Chen <shihjay2@gmail.com>
2024-10-29 23:25:22 -0700

    update privacy policy

--- components/layout.tsx
+++ components/layout.tsx
@@ -118,9 +118,20 @@ const Layout = (props: any) => {
       setTitle('Privacy Policy');
       setContent([
         'We collect emails to contact and identify you but we do not share them with anyone.',
-        'We use Stripe. We do not have access to your credit card information.',
         'We do not read or use your information except as directed by policies you can customize.',
-        'Trustee Community issues access credentials based on active Doximity accounts.'
+        'We do not disclose your information except as directed by policies you can customize.',
+        "Trustee Community issues access credentials based on a specific email invitation or a patient’s policy linked to  active Doximity accounts.",
+        'Patient data in the cloud is secured through encryption in transit and at rest. Access and policy enforcement are based on the IETF RFC 9635 protocol. Access authorization is secured with Passkeys to prevent password phishing and sharing.',
+        'Data retention is entirely patient-controlled. Patients can easily delete their health record data at any time, leaving only their contact email in our files. We do not review, share or use invitation email addresses or other access authorization policies except for the specific purpose of access authorization.',
+        'Trustee clinical data and authorization services are managed through typical hosting accounts at Digital Ocean, Inc. or Netlify, Inc. Neither HIE of One or our hosting providers share data with third-parties.',
+        'As a free and non-commercial demonstration, Trustee accounts may be closed and data deleted at any time. Users are encouraged to make and keep local copies on their computer or mobile device.',
+        'Trustee protects against unintended or overly broad data sharing in multiple ways:',
+        'Patients have fine-grained control over health record segments they capture from hospital records via SMART on FHIR',
+        'As a free service, patients concerned about family access demands can easily create alternate health records by simply using a different email address.',
+        'The use of passkeys instead of passwords discourages requests for password sharing.',
+        'Patients also have fine-grained control over data shared through invited access via email or via policy.',
+        'Patients have access to synthetic data files as a “sandbox” to help them better understand health record and sharing functionality before using Trustee with real patient data.',
+        'Restriction or withdrawal of an invited email address and changes in policy-based access are done on-line and effective immediately.'
       ]);
     }
     if (type == 'support') {
@@ -225,11 +236,15 @@ const Layout = (props: any) => {
           {title}
         </LayoutDialogTitle>
         <DialogContent dividers>
-          {content.map((text: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined, index: any) => (
-            <Typography gutterBottom key={index}>
-              {text}
-            </Typography>
-          ))}
+          <ul>
+            {content.map((text: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined, index: any) => (
+              <li key={index}>
+                <Typography gutterBottom>
+                  {text}
+                </Typography>
+              </li>
+            ))}
+          </ul>
         </DialogContent>
       </LayoutDialog>
       <Main open={openDrawer}>
--- pages/myTrustee/PolicySummary.tsx
+++ pages/myTrustee/PolicySummary.tsx
@@ -13,13 +13,48 @@ const PolicySummary = (props:any) => {
             We collect emails to contact and identify you but we do not share them with anyone.
           </li>
           <li>
-            We use Stripe. We do not have access to your credit card information.
-          </li>
-          <li>
             We do not read or use your information except as directed by policies you can customize.
           </li>
           <li>
-            Trustee Community issues access credentials based on active Doximity accounts.
+            We do not disclose your information except as directed by policies you can customize.
+          </li>
+          <li>
+            Trustee Community issues access credentials based on a specific email invitation or a patient’s policy linked to  active Doximity accounts.
+          </li>
+          <li>
+            Patient data in the cloud is secured through encryption in transit and at rest. Access and policy enforcement are based on the IETF RFC 9635 protocol. Access authorization is secured with Passkeys to prevent password phishing and sharing.
+          </li>
+          <li>
+            Data retention is entirely patient-controlled. Patients can easily delete their health record data at any time, leaving only their contact email in our files. We do not review, share or use invitation email addresses or other access authorization policies except for the specific purpose of access authorization.
+          </li>
+          <li>
+            Trustee clinical data and authorization services are managed through typical hosting accounts at Digital Ocean, Inc. or Netlify, Inc. Neither HIE of One or our hosting providers share data with third-parties.
+          </li>
+          <li>
+            As a free and non-commercial demonstration, Trustee accounts may be closed and data deleted at any time. Users are encouraged to make and keep local copies on their computer or mobile device.
+          </li>
+          <li>
+            Trustee protects against unintended or overly broad data sharing in multiple ways:
+            <ul>
+              <li>
+                Patients have fine-grained control over health record segments they capture from hospital records via SMART on FHIR,
+              </li>
+              <li>
+                As a free service, patients concerned about family access demands can easily create alternate health records by simply using a different email address. 
+              </li>
+              <li>
+                The use of passkeys instead of passwords discourages requests for password sharing.
+              </li>
+              <li>
+                Patients also have fine-grained control over data shared through invited access via email or via policy.
+              </li>
+              <li>
+                Patients have access to synthetic data files as a “sandbox” to help them better understand health record and sharing functionality before using Trustee with real patient data.
+              </li>
+            </ul>
+          </li>
+          <li>
+            Restriction or withdrawal of an invited email address and changes in policy-based access are done on-line and effective immediately. 
           </li>
         </ul>
         <h4>Optional Policies Available:</h4>
