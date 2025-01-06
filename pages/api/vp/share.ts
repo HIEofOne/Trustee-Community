3d1f627b421e8cc5190bcc9c0818723d380ee167
Michael Shihjay Chen <shihjay2@gmail.com>
2025-01-06 06:00:54 -0800

    debug

--- pages/api/vp/share.ts
+++ pages/api/vp/share.ts
@@ -48,11 +48,6 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
                 "id": "1",
                 "name": doc.vc_type + " Verifiable Credential",
                 "purpose": "We want a VC of this type to proof provider claim",
-                "schema": [
-                  {
-                    "uri": "VerifiableCredential"
-                  }
-                ],
                 "format": {
                   "jwt_vc_json": {
                     "alg": [
